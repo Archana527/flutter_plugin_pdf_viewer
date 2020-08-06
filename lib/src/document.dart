d560f91d791132fd41db78d9fe9e5291e8c19fcf
Archana527 <archana.onerooftech@gmail.com>
2020-08-06 15:56:03 +0530

    commiting

--- lib/src/document.dart
+++ lib/src/document.dart
@@ -1,12 +1,11 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+import 'package:flutter_plugin_pdf_viewer/src/page.dart';
 import 'package:path_provider/path_provider.dart';
-
-import '../flutter_plugin_pdf_viewer.dart';
+import 'package:rxdart/rxdart.dart';
 
 class PDFDocument {
   static const MethodChannel _channel =
@@ -14,10 +13,6 @@ class PDFDocument {
 
   String _filePath;
   int count;
-  List<PDFPage> pages = [];
-  List<Image> images = [];
-  bool _preloaded = false;
-  bool _imagePreloaded = false;
 
   /// Load a PDF File from a given File
   ///
@@ -28,8 +23,7 @@ class PDFDocument {
     try {
       var pageCount =
           await _channel.invokeMethod('getNumberOfPages', {'filePath': f.path});
-      // document.count = document.count = int.parse(pageCount);
-      document.count = int.parse(pageCount);
+      document.count = document.count = int.parse(pageCount);
     } catch (e) {
       throw Exception('Error reading PDF!');
     }
@@ -39,16 +33,15 @@ class PDFDocument {
   /// Load a PDF File from a given URL.
   /// File is saved in cache
   ///
-  static Future<PDFDocument> fromURL(String url,
-      {Map<String, String> headers}) async {
+  static Future<PDFDocument> fromURL(String url) async {
     // Download into cache
-    File f = await DefaultCacheManager().getSingleFile(url, headers: headers);
+    File f = await DefaultCacheManager().getSingleFile(url);
     PDFDocument document = PDFDocument();
     document._filePath = f.path;
     try {
       var pageCount =
           await _channel.invokeMethod('getNumberOfPages', {'filePath': f.path});
-      document.count = int.parse(pageCount);
+      document.count = document.count = int.parse(pageCount);
     } catch (e) {
       throw Exception('Error reading PDF!');
     }
@@ -75,7 +68,7 @@ class PDFDocument {
     try {
       var pageCount = await _channel
           .invokeMethod('getNumberOfPages', {'filePath': file.path});
-      document.count = int.parse(pageCount);
+      document.count = document.count = int.parse(pageCount);
     } catch (e) {
       throw Exception('Error reading PDF!');
     }
@@ -85,104 +78,20 @@ class PDFDocument {
   /// Load specific page
   ///
   /// [page] defaults to `1` and must be equal or above it
-  Future<PDFPage> get({
-    int page = 1,
-    final Function(double) onZoomChanged,
-    final int zoomSteps,
-    final double minScale,
-    final double maxScale,
-    final double panLimit,
-  }) async {
+  Future<PDFPage> get({int page = 1}) async {
     assert(page > 0);
-    if (_preloaded && pages.isNotEmpty) return pages[page - 1];
     var data = await _channel
         .invokeMethod('getPage', {'filePath': _filePath, 'pageNumber': page});
-    return new PDFPage(
-      data,
-      page,
-      onZoomChanged: onZoomChanged,
-      zoomSteps: zoomSteps,
-      minScale: minScale,
-      maxScale: maxScale,
-      panLimit: panLimit,
-    );
-  }
-
-  Future<Image> getImage({
-    int page = 1
-  }) async {
-    assert(page > 0);
-    if(_imagePreloaded && images[page - 1] != null) return images[page - 1];
-    var data = await _channel
-        .invokeMethod('getPage', {'filePath': _filePath, 'pageNumber': page});
-    return new Image.file(File(data));
-  }
-
-  Future<void> preloadPages({
-    final Function(double) onZoomChanged,
-    final int zoomSteps,
-    final double minScale,
-    final double maxScale,
-    final double panLimit,
-  }) async {
-    int countvar = 1;
-    if(_preloaded || pages.length != 0) return;
-    pages = List(count);
-    await Future.forEach<int>(List(count), (i) async {
-      final data = await _channel.invokeMethod(
-          'getPage', {'filePath': _filePath, 'pageNumber': countvar});
-      pages[countvar - 1] = PDFPage(
-        data,
-        countvar,
-        onZoomChanged: onZoomChanged,
-        zoomSteps: zoomSteps,
-        minScale: minScale,
-        maxScale: maxScale,
-        panLimit: panLimit,
-      );
-      countvar++;
-    });
-    _preloaded = true;
-  }
-
-  Future<void> preloadImages() async {
-    int countvar = 1;
-    if(_imagePreloaded && images != null) return;
-    images = List(count);
-    await Future.forEach<int>(List(count), (_) async {
-      if(images == null) return;
-      final data = await _channel.invokeMethod(
-        'getPage', {'filePath': _filePath, 'pageNumber': countvar});
-      if(images != null) images[countvar - 1] = Image.file(File(data));
-      countvar++;
-    });
-    _imagePreloaded = true;
+    return new PDFPage(data, page);
   }
 
   // Stream all pages
-  Stream<PDFPage> getAll({final Function(double) onZoomChanged}) {
+  Observable<PDFPage> getAll() {
     return Future.forEach<PDFPage>(List(count), (i) async {
+      print(i);
       final data = await _channel
           .invokeMethod('getPage', {'filePath': _filePath, 'pageNumber': i});
-      return new PDFPage(
-        data,
-        1,
-        onZoomChanged: onZoomChanged,
-      );
+      return new PDFPage(data, 1);
     }).asStream();
-  }
-
-  void clearImageCache() {
-    if(images != null && images.length != 0){
-      for(var i = 0; i < images.length; i++) {
-        if(images[i] != null) images[i].image.evict();
-      }
-      images = null;
-    }
-    _imagePreloaded = false;
-  }
-
-  void clearFileCache() async {
-    await _channel.invokeMethod('clearCache');
   }
 }
--- lib/src/viewer.dart
+++ lib/src/viewer.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_plugin_pdf_viewer/src/tooltip.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
 import 'package:numberpicker/numberpicker.dart';
-
-import '../flutter_plugin_pdf_viewer.dart';
+import 'tooltip.dart';
 
 enum IndicatorPosition { topLeft, topRight, bottomLeft, bottomRight }
 
@@ -15,42 +14,17 @@ class PDFViewer extends StatefulWidget {
   final bool showPicker;
   final bool showNavigation;
   final PDFViewerTooltip tooltip;
-  final bool enableSwipeNavigation;
-  final Axis scrollDirection;
-  final bool lazyLoad;
-  final PageController controller;
-  final int zoomSteps;
-  final double minScale;
-  final double maxScale;
-  final double panLimit;
-
-  final Widget Function(
-    BuildContext,
-    int pageNumber,
-    int totalPages,
-    void Function({int page}) jumpToPage,
-    void Function({int page}) animateToPage,
-  ) navigationBuilder;
 
   PDFViewer(
       {Key key,
       @required this.document,
-      this.scrollDirection,
-      this.lazyLoad = true,
       this.indicatorText = Colors.white,
       this.indicatorBackground = Colors.black54,
       this.showIndicator = true,
       this.showPicker = true,
       this.showNavigation = true,
-      this.enableSwipeNavigation = true,
       this.tooltip = const PDFViewerTooltip(),
-      this.navigationBuilder,
-      this.controller,
-      this.indicatorPosition = IndicatorPosition.topRight,
-      this.zoomSteps,
-      this.minScale,
-      this.maxScale,
-      this.panLimit})
+      this.indicatorPosition = IndicatorPosition.topRight})
       : super(key: key);
 
   _PDFViewerState createState() => _PDFViewerState();
@@ -58,105 +32,47 @@ class PDFViewer extends StatefulWidget {
 
 class _PDFViewerState extends State<PDFViewer> {
   bool _isLoading = true;
-  int _pageNumber;
-  bool _swipeEnabled = true;
-  List<PDFPage> _pages;
-  PageController _pageController;
-  final Duration animationDuration = Duration(milliseconds: 200);
-  final Curve animationCurve = Curves.easeIn;
-
-  @override
-  void initState() {
-    super.initState();
-    _pages = List(widget.document.count);
-    _pageController = widget.controller ?? PageController();
-    _pageNumber = _pageController.initialPage + 1;
-    if(!widget.lazyLoad){
-      if(mounted) setState(() { _isLoading = true; });
-      Future.delayed(Duration.zero, () async {
-        await widget.document.preloadPages(
-          onZoomChanged: onZoomChanged,
-          zoomSteps: widget.zoomSteps,
-          minScale: widget.minScale,
-          maxScale: widget.maxScale,
-          panLimit: widget.panLimit,
-        );
-        _pages = widget.document.pages;
-        if(mounted) setState(() { _isLoading = false; });
-      });
-    }
-  }
+  int _pageNumber = 1;
+  int _oldPage = 0;
+  PDFPage _page;
+  List<PDFPage> _pages = List();
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    _pageNumber = _pageController.initialPage + 1;
+    _oldPage = 0;
+    _pageNumber = 1;
     _isLoading = true;
-    _pages = List(widget.document.count);
-    // _loadAllPages();
+    _pages.clear();
     _loadPage();
   }
 
   @override
   void didUpdateWidget(PDFViewer oldWidget) {
     super.didUpdateWidget(oldWidget);
-  }
-
-  @override
-  void dispose(){
-    if(widget.controller != null) widget.controller.dispose();
-    widget.document.clearFileCache();
-    super.dispose();
-  }
-
-  onZoomChanged(double scale) {
-    if (scale != 1.0) {
-      setState(() {
-        _swipeEnabled = false;
-      });
-    } else {
-      setState(() {
-        _swipeEnabled = true;
-      });
-    }
+    _oldPage = 0;
+    _pageNumber = 1;
+    _isLoading = true;
+    _pages.clear();
+    _loadPage();
   }
 
   _loadPage() async {
-    var num = _pageNumber;
-    if (_pages[num - 1] != null) return;
-    if (!widget.lazyLoad) return;
-    setState(() {
-      _isLoading = true;
-    });
-    final data = await widget.document.get(
-      page: num,
-      onZoomChanged: onZoomChanged,
-      zoomSteps: widget.zoomSteps,
-      minScale: widget.minScale,
-      maxScale: widget.maxScale,
-      panLimit: widget.panLimit,
-    );
-    _pages[num - 1] = data;
-    if (mounted) {
-      setState(() {
-        _isLoading = false;
-      });
+    setState(() => _isLoading = true);
+    if (_oldPage == 0) {
+      _page = await widget.document.get(page: _pageNumber);
+    } else if (_oldPage != _pageNumber) {
+      _oldPage = _pageNumber;
+      _page = await widget.document.get(page: _pageNumber);
     }
-  }
-
-  _animateToPage({int page}) {
-    _pageController.animateToPage(page != null ? page : _pageNumber - 1,
-        duration: animationDuration, curve: animationCurve);
-  }
-
-  _jumpToPage({int page}) {
-    _pageController.jumpToPage(page != null ? page : _pageNumber - 1);
+    if(this.mounted) {
+      setState(() => _isLoading = false);
+    }
   }
 
   Widget _drawIndicator() {
     Widget child = GestureDetector(
-        onTap:
-            widget.showPicker && widget.document.count > 1 ? _pickPage : null,
+        onTap: _pickPage,
         child: Container(
             padding:
                 EdgeInsets.only(top: 4.0, left: 16.0, bottom: 4.0, right: 16.0),
@@ -197,7 +113,7 @@ class _PDFViewerState extends State<PDFViewer> {
         }).then((int value) {
       if (value != null) {
         _pageNumber = value;
-        _jumpToPage();
+        _loadPage();
       }
     });
   }
@@ -207,31 +123,13 @@ class _PDFViewerState extends State<PDFViewer> {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          PageView.builder(
-            physics: _swipeEnabled && widget.enableSwipeNavigation
-                ? AlwaysScrollableScrollPhysics()
-                : NeverScrollableScrollPhysics(),
-            onPageChanged: (page) {
-              setState(() {
-                _pageNumber = page + 1;
-              });
-              _loadPage();
-            },
-            scrollDirection: widget.scrollDirection ?? Axis.horizontal,
-            controller: _pageController,
-            itemCount: _pages?.length ?? 0,
-            itemBuilder: (context, index) => _pages[index] == null
-                ? Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : _pages[index],
-          ),
+          _isLoading ? Center(child: CircularProgressIndicator()) : _page,
           (widget.showIndicator && !_isLoading)
               ? _drawIndicator()
               : Container(),
         ],
       ),
-      floatingActionButton: widget.showPicker && widget.document.count > 1
+      floatingActionButton: widget.showPicker
           ? FloatingActionButton(
               elevation: 4.0,
               tooltip: widget.tooltip.jump,
@@ -242,80 +140,64 @@ class _PDFViewerState extends State<PDFViewer> {
             )
           : null,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: (widget.showNavigation && widget.document.count > 1)
-          ? widget.navigationBuilder != null
-              ? widget.navigationBuilder(
-                  context,
-                  _pageNumber,
-                  widget.document.count,
-                  _jumpToPage,
-                  _animateToPage,
-                )
-              : BottomAppBar(
-                  child: new Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Expanded(
-                        child: IconButton(
-                          icon: Icon(Icons.first_page),
-                          tooltip: widget.tooltip.first,
-                          onPressed: _pageNumber == 1
-                              ? null
-                              : () {
-                                  _pageNumber = 1;
-                                  _jumpToPage();
-                                },
-                        ),
-                      ),
-                      Expanded(
-                        child: IconButton(
-                          icon: Icon(Icons.chevron_left),
-                          tooltip: widget.tooltip.previous,
-                          onPressed: _pageNumber == 1
-                              ? null
-                              : () {
-                                  _pageNumber--;
-                                  if (1 > _pageNumber) {
-                                    _pageNumber = 1;
-                                  }
-                                  _animateToPage();
-                                },
-                        ),
-                      ),
-                      widget.showPicker
-                          ? Expanded(child: Text(''))
-                          : SizedBox(width: 1),
-                      Expanded(
-                        child: IconButton(
-                          icon: Icon(Icons.chevron_right),
-                          tooltip: widget.tooltip.next,
-                          onPressed: _pageNumber == widget.document.count
-                              ? null
-                              : () {
-                                  _pageNumber++;
-                                  if (widget.document.count < _pageNumber) {
-                                    _pageNumber = widget.document.count;
-                                  }
-                                  _animateToPage();
-                                },
-                        ),
-                      ),
-                      Expanded(
-                        child: IconButton(
-                          icon: Icon(Icons.last_page),
-                          tooltip: widget.tooltip.last,
-                          onPressed: _pageNumber == widget.document.count
-                              ? null
-                              : () {
-                                  _pageNumber = widget.document.count;
-                                  _jumpToPage();
-                                },
-                        ),
-                      ),
-                    ],
+      bottomNavigationBar: (widget.showNavigation || widget.document.count > 1)
+          ? BottomAppBar(
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: IconButton(
+                      icon: Icon(Icons.first_page),
+                      tooltip: widget.tooltip.first,
+                      onPressed: () {
+                        _pageNumber = 1;
+                        _loadPage();
+                      },
+                    ),
                   ),
-                )
-          : null,
+                  Expanded(
+                    child: IconButton(
+                      icon: Icon(Icons.chevron_left),
+                      tooltip: widget.tooltip.previous,
+                      onPressed: () {
+                        _pageNumber--;
+                        if (1 > _pageNumber) {
+                          _pageNumber = 1;
+                        }
+                        _loadPage();
+                      },
+                    ),
+                  ),
+                  widget.showPicker
+                      ? Expanded(child: Text(''))
+                      : SizedBox(width: 1),
+                  Expanded(
+                    child: IconButton(
+                      icon: Icon(Icons.chevron_right),
+                      tooltip: widget.tooltip.next,
+                      onPressed: () {
+                        _pageNumber++;
+                        if (widget.document.count < _pageNumber) {
+                          _pageNumber = widget.document.count;
+                        }
+                        _loadPage();
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: IconButton(
+                      icon: Icon(Icons.last_page),
+                      tooltip: widget.tooltip.last,
+                      onPressed: () {
+                        _pageNumber = widget.document.count;
+                        _loadPage();
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : Container(),
     );
   }
 }
